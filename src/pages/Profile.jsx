064c25e0dab4384fbedede912576b1dc0e8bb60f
quanphu1902 <88502952+quanphu1902@users.noreply.github.com>
2023-11-03 16:34:20 +0700

    add the edit funtion to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
-import  {getAuth} from "firebase/auth"
+import  {getAuth, updateProfile} from "firebase/auth"
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { doc, updateDoc } from 'firebase/firestore'
+import { db } from '../firebase'
 export default function Profile() {
   const auth = getAuth()
   const navigate = useNavigate()
+  const [changeDetail ,setChangeDetail] = useState(false)
   const [formData,setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
@@ -17,6 +21,36 @@ export default function Profile() {
       navigate("/")
 
   } 
+  function onChange(e){
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+
+  }
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //update display name in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // update name in the firestore
+
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Profile details updated");
+    } catch (error) {
+      toast.error("Could not update the profile details");
+    }
+  }
+    
+
+  
   
   return (
     
@@ -27,8 +61,10 @@ export default function Profile() {
       <div className="w-full md:w-[50%] mt-6 px-3">
         <form>
         { }
-        <input type='text' id="name" value={name} disabled className='mb-6 w-full px-4 py-2 text-xl text-gray-700
-        bg-white border border-gray-300 rounded transition ease-in-out '></input>
+        <input type='text' id="name" value={name} disabled={!changeDetail} onChange={onChange}
+         className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}></input>
 
         <input type='email' id="email" value={email} disabled className='mb-6 w-full px-4 py-2 text-xl text-gray-700
         bg-white border border-gray-300 rounded transition ease-in-out'></input>
@@ -36,8 +72,11 @@ export default function Profile() {
         <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
           <p className=" flex items-center ">
             Do you want change your name ? 
-            <span className='text-red-600 hover:text-red-700 transiton ease-in-out duration-200 ml-1
-            cursor-pointer'> Edit </span>
+            <span   onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }} className='text-red-600 hover:text-red-700 transiton ease-in-out duration-200 ml-1
+            cursor-pointer'> {changeDetail ? "Appy Change " : "Edit "} </span>
           </p>
         <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>
           Sign out
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -47,8 +47,8 @@ export default function SignUp() {
       formDataCopy.timestamp = serverTimestamp();
 
       await setDoc(doc(db, "users", user.uid), formDataCopy);
-      // toast.success("Sign up was successful");
-      // navigate("/");
+      toast.success("Sign up was successful");
+      navigate("/");
     } catch (error) {
       toast.error("Something went wrong with the registration");
     }
