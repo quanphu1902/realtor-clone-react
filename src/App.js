7d81798ead4cd652233d02104719d4249180b8ef
quanphu1902 <88502952+quanphu1902@users.noreply.github.com>
2023-11-30 15:08:40 +0700

    Create editlisting page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router , Routes ,Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -6,36 +6,36 @@ import SignUp from "./pages/SignUp";
 import PrivateRoute from "./components/PrivateRoute";
 import SignIn from "./pages/SignIn";
 import Offers from "./pages/Offers";
-import ForgotPassword from "./pages/ForgotPassword"
+import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/Header";
-import {ToastContainer} from "react-toastify"
+import { ToastContainer } from "react-toastify";
 import CreateListing from "./pages/CreateListing";
-
+import EditListing from "./pages/EditListing";
 function App() {
   return (
     <>
-    
-   <Router>
-     <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/profile" element={<PrivateRoute />}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
-
-      </Route>
-      <Route path="/sign-in" element={<SignIn/>}></Route>
-      <Route path="/Sign-up" element={<SignUp/>}></Route>
-      <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
-      <Route path="/offers" element={<Offers/>}></Route>
-      <Route path="create-listing" element = {<PrivateRoute/>}>
-      <Route path="/create-listing" element={<CreateListing/>}></Route>
-      </Route>
-
+          </Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/Sign-up" element={<SignUp />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route path="/offers" element={<Offers />}></Route>
+          <Route path="create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />}></Route>
+          </Route>
+          <Route path="edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
+          </Route>
       
-    </Routes>
-  
-   </Router>
-   <ToastContainer
+          
+        </Routes>
+      </Router>
+      <ToastContainer
         position="bottom-center"
         autoClose={5000}
         hideProgressBar={false}
@@ -47,11 +47,8 @@ function App() {
         pauseOnHover
         theme="dark"
       />
-
-    
-   </>
+    </>
   );
 }
-
 
 export default App;
